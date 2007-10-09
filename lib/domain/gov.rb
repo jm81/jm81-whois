900ac859627f48ad6e3a9402e875b61761d9f2bc
jmorgan <jmorgan@b3c654a4-2f21-44f8-ad75-3c2ab8062a1e>
2007-10-09 00:49:23 +0000

    Add Domain::Gov for .gov domains. Very minimal, really only returns status.

---
+++ lib/domain/gov.rb
@@ -0,0 +1,35 @@
+# All I know how to get so far is domain name and status.
+# There is a web interface at http://www.dotgov.gov/whois.aspx
+# with more detail.
+class Whois::Domain::Gov < Whois::Domain::Base
+  HOST = "whois.nic.gov"
+  ATTR_MATCH = /^([^:]+):\s(.*)\r$/
+  responds_to :gov
+
+  ATTR_NAMES = {
+  }
+  
+  def database_updated_at
+    nil
+  end
+  
+  def whois_server
+    HOST
+  end
+  
+  def available?
+    @raw !~ /Domain Name:/
+  end
+  
+  def register_url
+    "http://www.dotgov.gov/help_how.aspx"
+  end
+  
+  def name_servers
+    []
+  end
+  
+  def ns
+    []
+  end
+end
---
+++ test/domain/gov_test.rb
@@ -0,0 +1,30 @@
+require 'test/unit'
+require File.join(File.dirname(__FILE__), "..", "test_helper")
+require File.join(File.dirname(__FILE__), "..", "..", "lib", "whois")
+
+class Whois::Domain::GovTest < Test::Unit::TestCase
+
+  def test_whois  
+    assert_whois_data(EXP_DATA)
+    assert Whois::Domain.new("not-reg-1234.gov").available?
+  end
+  
+  def test_register_url
+    assert_equal "http://www.dotgov.gov/help_how.aspx", Whois::Domain.new("not-reg-1234.gov").register_url
+  end
+
+  EXP_DATA = {
+    :name => "dotgov.gov",
+    :kind => "Gov",
+    :created_on => nil,
+    :updated_on => nil,
+    :expires_on => nil,
+    :database_updated_at => false,
+    :registrar_name => nil,
+    :whois_server => "whois.nic.gov",
+    :raw_match => "Domain Name: dotgov.gov",
+    :status => ['Active'],
+    :name_servers => []
+  }
+
+end
