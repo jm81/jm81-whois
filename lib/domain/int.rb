8b35c8facf15906ebb3c9a9e34f17ca1914a5a7f
jmorgan <jmorgan@b3c654a4-2f21-44f8-ad75-3c2ab8062a1e>
2007-10-10 00:29:57 +0000

    Add Domain::Int for .int domains.

---
+++ lib/domain/int.rb
@@ -0,0 +1,41 @@
+class Whois::Domain::Int < Whois::Domain::Base
+  HOST = "whois.iana.org"
+  ATTR_MATCH = /^\s{0,4}([^:]+):\s+(\S.*)\r$/
+  responds_to :int
+
+  ATTR_NAMES = {
+    :registrar_name => "Sponsoring Registrar",
+    :created_on => "Registration Date",
+    :updated_on => "Last Updated Date",
+    :name_servers => "Nameserver"
+  }
+  
+  def database_updated_at
+    nil
+  end
+  
+  def whois_server
+    HOST
+  end
+  
+  def registrar_name
+    ".int Domain Registry (IANA)"
+  end
+  
+  def available?
+    @raw =~ /not found/
+  end
+  
+  def created_on
+    Date.parse(attrs["Registration Date"][-1])
+  end
+
+  def updated_on
+    Date.parse(attrs["Last Updated Date"][-1])
+  end
+  
+  def name_servers
+    # Remove trailing dot.
+    super.collect{ |ns| ns[0..-2] }
+  end
+end
---
+++ test/domain/int_test.rb
@@ -0,0 +1,26 @@
+require 'test/unit'
+require File.join(File.dirname(__FILE__), "..", "test_helper")
+require File.join(File.dirname(__FILE__), "..", "..", "lib", "whois")
+
+class Whois::Domain::IntTest < Test::Unit::TestCase
+
+  def test_whois  
+    assert_whois_data(EXP_DATA)
+    assert Whois::Domain.new("not-reg-1234.int").available?
+  end
+
+  EXP_DATA = {
+    :name => "un.int",
+    :kind => "Int",
+    :created_on => "1997-04-23",
+    :updated_on => "2006-08-14",
+    :expires_on => nil,
+    :database_updated_at => false,
+    :registrar_name => ".int Domain Registry (IANA)",
+    :whois_server => "whois.iana.org",
+    :raw_match => "Domain: un.int",
+    :status => [],
+    :name_servers => ['unhqint1.un.org', 'ns.uu.net']
+  }
+
+end
