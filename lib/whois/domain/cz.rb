094fb3ec2b7b3f6f2940335d28213188e812398c
Kepi <kepi@igloonet.cz>
2009-12-02 04:36:23 +0800

    Add Whois::Domain::Cz for .cz

---
+++ lib/whois/domain/cz.rb
@@ -0,0 +1,31 @@
+class Whois::Domain::Cz < Whois::Domain::Base
+  HOST = "whois.nic.cz"
+  ATTR_MATCH = /^([^:]+):\s+(\S.*)\r$/
+  responds_to :cz
+
+  ATTR_NAMES = {
+    :registrar_name => "registrar",
+    :created_on => "registered",
+    :updated_on => "changed",
+    :name_servers => "nserver",
+    :expires_on => "expire",
+    :status => "status",
+    :registrant_name => "registrant"
+  }
+
+  def query_string
+    "#{@name}\r\n"
+  end
+
+  def available?
+    @raw =~ /No entries found/
+  end
+
+  def name_servers
+    attr_array(:name_servers).map {|ns| ns.gsub(/\s+.*$/, "") }
+  end
+
+  def register_url
+    "http://www.nic.cz/page/313/"
+  end
+end
