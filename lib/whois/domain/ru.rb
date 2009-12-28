df6b9f47d0afbd8df71ad75f504b5b0f1cda3e16
Ivo Dimitrov <darwinium@gmail.com>
2009-12-28 21:43:20 +0500

    add .ru zone

---
+++ lib/whois/domain/ru.rb
@@ -0,0 +1,17 @@
+﻿class Whois::Domain::Ru < Whois::Domain::Base
+  HOST = "whois.ripn.net"
+  ATTR_MATCH = /\s*([^:]+):\s+(.*)$/
+  responds_to :ru
+
+  ATTR_NAMES = {
+    :registrar_name => "Registrar Name",
+    :whois_server => "Registrar Whois",
+    :created_on => "Domain Created",
+    :updated_on => "Domain Last Updated",
+    :expires_on => "Domain Currently Expires"
+  }
+  
+  def available?
+    @raw =~ /NOT FOUND/
+  end
+end
