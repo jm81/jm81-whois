29cb1c3ff7bf634e93d2f38802b36175de61607c
Ivo Dimitrov <darwinium@gmail.com>
2009-12-28 21:57:21 +0500

    add .su zone

---
+++ lib/whois/domain/su.rb
@@ -0,0 +1,17 @@
+﻿class Whois::Domain::Su < Whois::Domain::Base
+  HOST = "whois.ripn.net"
+  ATTR_MATCH = /\s*([^:]+):\s+(.*)$/
+  responds_to :su
+
+  ATTR_NAMES = {
+    :registrar_name => "Registrar Name",
+    :whois_server => "Registrar Whois",
+    :created_on => "Domain Created",
+    :updated_on => "Domain Last Updated",
+    :expires_on => "Domain Currently Expires"
+  }
+  
+  def available?
+    @raw =~ /NOT FOUND/
+  end
+end
